d35d3f457685a9f9dc5a71726047832082455a7b
nistros <antonykloss7@gmail.com>
2026-07-30 12:20:18 -0300

    Luana: Trocada a cor do fundo

--- biblioteca-mobile/src/app/lua2.tsx
+++ biblioteca-mobile/src/app/lua2.tsx
@@ -3,12 +3,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Lua2Screen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Página 2: Lua 2 ✨</Text>
-    </View>
+         <View style={styles.card}>
+           <Text style={styles.title}>Página 2: Lua 2 ✨</Text>
+         </View>
+       </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgb(184, 203, 210)' },
   title: { fontSize: 22, fontWeight: 'bold' },
+  card: {
+    width: '90%',
+    maxWidth: 500,
+    padding: 24,
+    backgroundColor: '#e1d5d5',
+    borderRadius: 12,
+    alignItems: 'center',
+  },
 });
